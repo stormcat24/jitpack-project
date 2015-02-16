28a5c4c08a75bb7ea6cf71e1853b0bef32536836
stormcat24 <a.yamada24@gmail.com>
2015-02-16 23:30:00 +0900

    Initial Commit

---
+++ src/main/java/JitpackSandbox.java
@@ -0,0 +1,13 @@
+import stormcat24.jitpack.Library;
+
+/**
+ * @author stormcat24
+ */
+public class JitpackSandbox {
+
+    public static void main(String[] args) {
+
+        Library.foo();
+        Library.bar();
+    }
+}
